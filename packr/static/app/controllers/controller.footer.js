32fb4583856dfbd203d6c66d2bf3309b9a51114d
Matthew Miller <mnmiller1@me.com>
2016-10-13 11:16:54 +1000

    Made the about button work.

--- packr/static/app/controllers/controller.footer.js
+++ packr/static/app/controllers/controller.footer.js
@@ -1,10 +1,13 @@
 (function () {
     'use strict';
 
-    var deps = ['$scope', '$location', '$mdFooter'];
-    function footerController($scope, $location, $mdFooter) {
+    var deps = ['$scope', '$location'];
+    function footerController($scope, $location) {
         var self = this; // jshint ignore:line
 
+        self.goAbout = function () {
+            $location.path('/about');
+        };
     }
 
     footerController.$inject = deps;
