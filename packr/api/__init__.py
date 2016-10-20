b28675912e66884cd3b3bf2343ca5c800d0339a2
Matthew Miller <mnmiller1@me.com>
2016-10-20 10:02:31 +1000

    Implement the quote API, and hook it up to the page.

--- packr/api/__init__.py
+++ packr/api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restplus import Api
 from .user import api as user_api
+from .quote import api as quote_api
 
 blueprint = Blueprint('api', __name__)
 api = Api(blueprint,
@@ -8,3 +9,4 @@ api = Api(blueprint,
           version='0.0.1',
           description='For sending packages')
 api.add_namespace(user_api)
+api.add_namespace(quote_api)
