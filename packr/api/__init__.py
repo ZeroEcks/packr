fe333b9b99fc78bac6546ae9895e37ca967f9b27
Matthew Miller <mnmiller1@me.com>
2016-10-21 17:03:05 +1000

    Added the booking API and hooked it up.

--- packr/api/__init__.py
+++ packr/api/__init__.py
@@ -3,6 +3,7 @@ from flask_restplus import Api
 from .user import api as user_api
 from .quote import api as quote_api
 from .track import api as track_api
+from .book import api as book_api
 
 blueprint = Blueprint('api', __name__)
 api = Api(blueprint,
@@ -12,3 +13,4 @@ api = Api(blueprint,
 api.add_namespace(user_api)
 api.add_namespace(quote_api)
 api.add_namespace(track_api)
+api.add_namespace(book_api)
