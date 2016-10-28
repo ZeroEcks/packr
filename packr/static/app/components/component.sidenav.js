62d7e6f0a995e238970a4be3a060c79973b0dbfa
Heath <heath.mayocchi@qut.connect.edu.au>
2016-10-28 13:25:27 +1000

    n9378201-portfolio

--- packr/static/app/components/component.sidenav.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('App').component('sidenav', {
-        templateUrl: 'static/views/sidenav.html',
-        controller: 'SidenavController'
-    });
-})();
